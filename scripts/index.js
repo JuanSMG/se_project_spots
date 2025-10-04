a3115fbbfe95624e05739ccd1f060111aeec618e
Juan Gonzalez <juan_sm_101@yahoo.com>
2025-10-04 00:24:19 -0400

    summit sprint project stage 5

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,27 @@ const newPostAddBtn = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 
+const profileHeadingEl = document.querySelector(".profile__heading");
+const profileDescriptionEl = document.querySelector(".profile__discription");
+
+const profileNameInput = editProfileModal.querySelector("#profile-name-input");
+const profileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const profileFormElement = editProfileModal.querySelector(".modal__form");
+
+const newPostImageInput = newPostModal.querySelector("#profile-image-input");
+const newPostCaptionInput = newPostModal.querySelector(
+  "#profile-caption-input"
+);
+
+const newPostFormElement = newPostModal.querySelector(".modal__form");
+
 profileEditBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
+
+  profileNameInput.value = profileHeadingEl.textContent;
+  profileDescriptionInput.value = profileDescriptionEl.textContent;
 });
 
 profileCloseBtn.addEventListener("click", function () {
@@ -21,3 +40,25 @@ newPostAddBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileHeadingEl.textContent = profileNameInput.value;
+  profileDescriptionEl.textcontent = profileDescriptionInput.value;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function handleNewPostFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log("newPostImageInput");
+  console.log("newPostCaptionInput");
+
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostFormElement.addEventListener("submit", handleNewPostFormSubmit);
